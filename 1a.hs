4ad51b2cb009c085593ecaaa5059009a788b0d99
Kate Dawson <dawsonka@chesterfield.ac.uk>
2019-01-29 14:13:25 +0000

    Find duplicates

--- 1a.hs
+++ 1a.hs
@@ -1,3 +1,4 @@
+import qualified Data.Set     as Set
 import qualified Data.Text    as Text
 import qualified Data.Text.IO as Text
 
@@ -7,10 +8,13 @@ main = do
         ints = map stringToInt ss
         foreverInts = cycle ints
         partialSums = scanl (+) 0 foreverInts
-    putStrLn $ show  partialSums
+    putStrLn $ show  $ isDuplicate partialSums Set.empty
 
 stringToInt s 
     | head s == '+' = stringToInt $ tail s
     | otherwise       = read s::Int
 
+isDuplicate (x:xs) s
+    | Set.member x s = x
+    | otherwise      = isDuplicate xs $ Set.insert x s
 
