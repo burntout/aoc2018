f607116f3d66e1ff7d205062f03a76ed75d27367
Kate Dawson <dawsonka@chesterfield.ac.uk>
2019-02-04 14:56:40 +0000

    broken

--- 3a.hs
+++ 3a.hs
@@ -17,7 +17,7 @@ main = do
     -- putStrLn $ show $ findNonOverlapping claims
     -- putStrLn $ show $ findNonOverlap claims
     --    c = getClaim 88 claims
-    putStrLn  $ show $ fno claims
+    putStrLn  $ show $ fno1 claims
 
 stringToClaim str = Claim { claimID = claimID, corner = (x,y), xLength = xl, yLength =  yl }
     where 
@@ -41,6 +41,26 @@ intersectClaims x y = Set.intersection xp yp
 hasOverlap x y = Set.empty /= intersectClaims x y 
 
 overlaps c cs = filter (hasOverlap c) cs
+
+corners c = [(x0,y0),( x1,y0), (x0,y1), (x1,y1)]
+    where 
+        (x0,y0) = corner c
+        (x1,y1) = (x0 + xLength c - 1, y0 + yLength c - 1)
+
+
+hasoverlaps1' a b  = and $ map (\x -> (inRange (ax0,ax1) (fst x) && (inRange (ay0, ay1) (snd x)))) $ corners b 
+    where
+        (ax0, ay0) =  corner a
+        (ax1,ay1) = (ax0 + xLength a - 1,  ay0 + yLength a - 1)
+        inRange (a,b) x  = (a <= x) && (x <= b)
+
+hasoverlaps1 a b = (hasoverlaps1' a b) || (hasoverlaps1' b a) 
+
+overlaps1 c cs = filter (hasoverlaps1 c) cs
+
+fno1 cs = [c | c <- cs, (length $ overlaps1 c cs) == 1]
+
+
 
 findNonOverlap (c:cs) 
     | overlaps c cs == [] = c
