2f83760218f2b106a98b2c7afd3178f7e96a57b6
Kate Dawson <kate@webarchitects.coop>
2019-02-05 22:30:57 +0000

    efficienter!

--- 3a.hs
+++ 3a.hs
@@ -14,10 +14,7 @@ main = do
     claimText <- fmap Text.lines (Text.readFile "3.txt")
     let claimStrings =  map Text.unpack claimText
         claims = map stringToClaim claimStrings
-    -- putStrLn $ show $ findNonOverlapping claims
-    -- putStrLn $ show $ findNonOverlap claims
-    --    c = getClaim 88 claims
-    putStrLn  $ show $ fno1 claims
+    putStrLn  $ show $ fno claims
 
 stringToClaim str = Claim { claimID = claimID, corner = (x,y), xLength = xl, yLength =  yl }
     where 
@@ -28,44 +25,29 @@ stringToClaim str = Claim { claimID = claimID, corner = (x,y), xLength = xl, yLe
         xl = read ((splitOn "x" $ ws!!3)!!0) :: Int
         yl = read ((splitOn "x" $ ws!!3)!!1) :: Int
 
-claimToPoints claim = Set.fromList [ (x,y) | x <- [x1 .. x2], y <- [y1 .. y2]]
-    where
-        (x1, y1) = corner claim
-        x2 = x1 + xLength claim - 1
-        y2 = y1 + yLength claim - 1
-
-intersectClaims x y = Set.intersection xp yp
-    where xp = claimToPoints x
-          yp = claimToPoints y
-
-hasOverlap x y = Set.empty /= intersectClaims x y 
-
-overlaps c cs = filter (hasOverlap c) cs
-
 corners c = [(x0,y0),( x1,y0), (x0,y1), (x1,y1)]
     where 
         (x0,y0) = corner c
         (x1,y1) = (x0 + xLength c - 1, y0 + yLength c - 1)
 
+hasoverlaps a b  = (x0 < x1) && (y0 < y1) 
+    where 
+        x0 = maximum $ map (fst . xcoords) [a, b]
+        x1 = minimum $ map (snd . xcoords) [a, b]
+        y0 = maximum $ map (fst . ycoords) [a, b]
+        y1 = minimum $ map (snd . ycoords) [a, b]
+        
+xcoords c = (cx0, cx1)
+    where 
+        cx0 = fst $  corner c
+        cx1 = cx0 + xLength c 
 
-hasoverlaps1' a b  = and $ map (\x -> (inRange (ax0,ax1) (fst x) && (inRange (ay0, ay1) (snd x)))) $ corners b 
-    where
-        (ax0, ay0) =  corner a
-        (ax1,ay1) = (ax0 + xLength a - 1,  ay0 + yLength a - 1)
-        inRange (a,b) x  = (a <= x) && (x <= b)
+ycoords c = (cy0, cy1)
+    where 
+        cy0 = snd $  corner c
+        cy1 = cy0 + yLength c 
+            
 
-hasoverlaps1 a b = (hasoverlaps1' a b) || (hasoverlaps1' b a) 
-
-overlaps1 c cs = filter (hasoverlaps1 c) cs
-
-fno1 cs = [c | c <- cs, (length $ overlaps1 c cs) == 1]
-
-
-
-findNonOverlap (c:cs) 
-    | overlaps c cs == [] = c
-    | otherwise =  findNonOverlap $ cs \\ overlaps c cs
-
-getClaim n cs = head $ filter (\x -> claimID x == n) cs
+overlaps c cs = filter (hasoverlaps c) cs
 
 fno cs = [c | c <- cs, (length $ overlaps c cs) == 1]
