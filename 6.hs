279c7ea196a921ef600502e71c88a444d58ec5df
Kate Dawson <kate@webarchitects.coop>
2019-02-14 23:24:19 +0000

    wrong

--- 6.hs
+++ 6.hs
@@ -7,7 +7,10 @@ main = do
     pointsText <- fmap Text.lines (Text.readFile "6.txt")
     let pointStrings = map Text.unpack pointsText
         points = map stringToPoint pointStrings 
-    putStrLn $ show $ points
+        allPointsToMeasure = getAllPointsToMeasure points
+        listOfClosestPoints = map (findClosestPoint points) allPointsToMeasure
+        getCountsOfClosestPoints = maximum [length $ filter (==(Just p)) listOfClosestPoints | p <- points]
+    putStrLn $ show $ getCountsOfClosestPoints
 
 data Point = Point { xcoord :: Int, ycoord :: Int } deriving (Eq, Show)
 
@@ -23,3 +26,25 @@ distance p1 p2 = xdiff + ydiff
     where
         xdiff = abs $ (xcoord  p1) - (xcoord p2)
         ydiff = abs $ (ycoord  p1) - (ycoord p2)
+
+getAllPointsToMeasure points = [ Point { xcoord = x, ycoord = y } | x <- [minX  .. maxX], y <- [minY .. maxY]]
+    where 
+        minX = minimum $ map xcoord points
+        minY = minimum $ map ycoord points
+        maxX = maximum $ map xcoord points
+        maxY = maximum $ map ycoord points
+
+findClosestPoint :: [Point]-> Point -> Maybe Point
+findClosestPoint ps p
+    | length closestPoints == 1  = Just $ head closestPoints
+    | otherwise                   = Nothing
+    where 
+        distanceToPoints = map (\x -> ((distance x p), x)) ps
+        minDistance = minimum $ map fst distanceToPoints
+        closestPoints = map snd $ filter (\x -> fst x == minDistance) distanceToPoints
+      
+
+
+
+
+
