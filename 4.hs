ce0b9e3104a8c916681d40a260ecc5385f6c2d7f
Kate Dawson <kate@webarchitects.coop>
2019-02-17 01:52:49 +0000

    Progress with the Control.State.Monad

--- 4.hs
+++ 4.hs
@@ -1,3 +1,4 @@
+import Control.Monad.State
 import Data.Char (isSpace)
 import Data.List
 import Data.List.Split
@@ -16,7 +17,10 @@ main = do
     dataText <- fmap Text.lines (Text.readFile "4.txt")
     let dataStrings =  map Text.unpack dataText
         logEntries = sort $ map dataStringToLogEntry dataStrings
-    putStrLn $ show $ map (getGuard . logText) $ filter (isGuardShift) logEntries
+        startState = (0, Map.empty)
+        logsByGuard = evalState (processLogs logEntries) startState
+    -- putStrLn $ show $ map (getGuard) $ filter (isGuardShift) logEntries
+    putStrLn $ show $ logsByGuard
   
 
 dataStringToLogEntry str = LogEntry {logTime = logTime, logText = logText}
@@ -36,22 +40,35 @@ isGuardShift logEntry = drop (length str - 12) str == "begins shift"
     where
         str = logText logEntry
 
-getGuard guardShift = read (tail $ (words guardShift)!!1)::Int
+getGuard logEntry = read (tail $ (words guardShift)!!1)::Int
+    where guardShift = logText logEntry 
 
 isStartSleep logEntry= (logText logEntry) == "falls asleep"
 
 isEndSleep logEntry = (logText logEntry) == "wakes up"
 
-arrangeLogEntries [] = [] 
-arrangeLogEntries (l:ls) 
-    | isGuardShift l = addGuardLog l ls
-    | otherwise arrangeLogEntries ls 
+appendLogEntry id entry dict = Map.insertWith (++) id entry dict
 
+-- Process the logEntries
+-- Used the following 
+-- https://wiki.haskell.org/State_Monad#Complete_and_Concrete_Example_1
 
+processLogs [] = do 
+    (_, guardTimes) <- get
+    return guardTimes 
 
--- addGuardLog g (a:b:cs)
---     | isStartSleep a  = 
---     | otherwise       = 
--- 
--- parseLogText
+processLogs (x:xs) = do
+    (guardID, guardTimes) <- get
+    case (logType x) of
+        "guard" -> put (getGuard x, guardTimes)
+        "sleep" -> put (guardID, appendLogEntry guardID [(logTime x,0)] guardTimes)
+        "wakes" -> put (guardID, appendLogEntry guardID [(logTime x,1)] guardTimes)
+        _       -> put (guardID, guardTimes)
+    processLogs xs
 
+logType x
+    | isGuardShift x = "guard"
+    | isStartSleep x = "sleep"
+    | isEndSleep x   = "wakes"
+
+-- getSleeps guardID guardLogs = guardLogs 
