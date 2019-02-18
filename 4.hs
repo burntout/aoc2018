b025014c3cd90868e75382530d1f8a54fec279f2
Kate Dawson <kate@webarchitects.coop>
2019-02-18 00:22:02 +0000

    More work in first part of Q4

--- 4.hs
+++ 4.hs
@@ -87,4 +87,12 @@ startSleep times = map fst $ filter (\x-> snd x == 0) times
 endSleep times = map fst $ filter (\x-> snd x == 1) times
 sleepLength times = sum $ zipWith (diffUTCTime) (endSleep times) (startSleep times)
 
+
+getMinuteOfTime :: UTCTime -> Integer
+getMinuteOfTime t = m
+    where 
+        UTCTime day seconds = t
+        m = (diffTimeToPicoseconds seconds) `quot` (60 * 10^12)
+
+
 testData = ["[1518-11-01 00:00] Guard #10 begins shift","[1518-11-01 00:05] falls asleep","[1518-11-01 00:25] wakes up","[1518-11-01 00:30] falls asleep","[1518-11-01 00:55] wakes up","[1518-11-01 23:58] Guard #99 begins shift","[1518-11-02 00:40] falls asleep","[1518-11-02 00:50] wakes up","[1518-11-03 00:05] Guard #10 begins shift","[1518-11-03 00:24] falls asleep","[1518-11-03 00:29] wakes up","[1518-11-04 00:02] Guard #99 begins shift","[1518-11-04 00:36] falls asleep","[1518-11-04 00:46] wakes up","[1518-11-05 00:03] Guard #99 begins shift","[1518-11-05 00:45] falls asleep","[1518-11-05 00:55] wakes up"]
