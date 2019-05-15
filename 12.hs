c5245a1ea3594156584e3ac0d7101dbef5fae2fc
Kate Dawson <kate@webarchitects.coop>
2019-05-16 00:38:55 +0100

    part 2 completed ..  could do with parser, and perhaps an iterated solution

--- 12.hs
+++ 12.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 import Data.List
+import Control.Monad.State
 
 inputData = "###..#...####.#..###.....####.######.....##.#####.##.##..###....#....##...##...##.#..###..#.#...#..#"
 
@@ -60,14 +63,29 @@ nextGenPlants growFunc xs
         front = [growFunc $ take 5 xs]
         back  = nextGenPlants growFunc $ tail xs
 
-processPlants growFunc (offset, plants) = trim $ (offset + 3, nextGenPlants growFunc $ "....." ++ plants ++ ".....")
-
 trim (offset, plants) = (offset - diff, drop diff newPlants)
     where
         diff = length $ takeWhile (== '.') plants
         newPlants = reverse $ dropWhile (== '.') $ reverse plants
 
+processPlants growFunc (offset, plants) = trim $ (offset + 3, nextGenPlants growFunc $ "....." ++ plants ++ ".....")
+
 result (offset, plants) = sum $ map (\x -> x - offset) $ elemIndices '#' plants
 
-main = print $ result $ last $ take 21 $ iterate (processPlants myF) (0, inputData)
+-- loop to find a stable plant pattern
+-- the pattern stays the same, but the offset alters
+-- returns (numGenerations, offsetDiff, (offset, plantPattern))
+findStablePattern growFunc = do
+    (genCnt, (offset, plants)) <- get
+    let (newOffset, newPlants) = processPlants growFunc (offset, plants)
+    put (genCnt + 1, (newOffset, newPlants))
+    if (plants == newPlants)
+        then return (genCnt + 1, newOffset - offset, (newOffset, newPlants))
+        else findStablePattern growFunc
 
+(generations, offsetDiff, (offset, plants)) = evalState (findStablePattern myF)  (0, (0, inputData))
+
+-- main = print $ result ((50000000000 - generations)*offsetDiff + offset, plants)
+
+-- Solution to Part 1 
+main = print $ result $ last $ take 21 $ iterate (processPlants myF) (0, inputData)
